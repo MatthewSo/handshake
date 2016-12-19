f5274c74ccedfc1178f130869408b2f7d6803921
darthbeep <darthbeep@gmail.com>
2016-12-19 03:19:41 -0500

    Add files via upload

--- pipe_network.c
+++ pipe_network.c
@@ -6,54 +6,55 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define WKP "What's red and bad for your teeth? A brick!"
+#define WKP2 "What's brown and sticky? A stick!!"
 
 
 int server_handshake(int * to_server){
-
-
-
+//Step 1
+  mkfifo(WKP, 0644);
   int in_fd = 0;
-printf("Server waiting for pipe\n");
-  //while (!in_fd){
-    in_fd = open("client-to-server", O_RDONLY);
-  //}
-
-  //char * recs = (char *) malloc(sizeof(char));
-  char recs[1000];
-  printf("%d\n", sizeof(recs));
-  read("client-to-server", recs, sizeof(recs));
+  printf("Server waiting for pipe\n");
+  //Step 2
+  *to_server = open(WKP, O_RDONLY);
+  //Step 6
+  char recs[MESSAGE_BUFFER_SIZE];
+  read(*to_server, recs, MESSAGE_BUFFER_SIZE);
   printf("Server recieved a pipe: %s\n", recs);
-  if (strcmp(recs, "recieved server-to-client") == 0) {
-    printf("Server sending pipe\n");
-    write("server-to-client", "recieved client-to-server", sizeof("recieved client-to-server"));
-    mkfifo("server-to-client", 0644);
-    open("server-to-client", O_WRONLY);
-  }
-  printf("Server made a mistake somewhere\n");
-  return getpid();
+  remove(WKP);
+  //Step 7
+  int to_client = open(recs, O_WRONLY);
+  char * sendback = (char *) malloc(sizeof(char));
+  sprintf(sendback, "%d", getpid());
+  printf("Server sending a pipe: %s\n", sendback);
+  write(to_client , sendback, strlen(sendback));
+  //Step 9
+  read(*to_server, recs, MESSAGE_BUFFER_SIZE);
+  printf("Server recieved: %s\n", recs);
+  return to_client;
 }
 
-int client_handshake(int * to_client) {
-  //int pid = getpid();
-  mkfifo("client-to-server", 0644);
-  int fd = open("client-to-server", O_WRONLY);
+int client_handshake(int * from_client) {
+  //Step 3
+  char * send = (char *) malloc(sizeof(char));
+  sprintf(send, "%d", getpid());
+  printf("Client sending a pipe: %s\n", send);
+  mkfifo(send, 0644);
+  //Step 4
+  * from_client = open(WKP, O_WRONLY);
   int in_fd = 0;
-  printf("Client sending a pipe\n");
-  char * send = "recieved server-to-client";
-  printf("%d\n", sizeof(send));
-  write("client-to-server", send, sizeof(send));
+
+  write(* from_client, send, sizeof(send));
   printf("Client waiting for pipe\n");
-  /*while (!in_fd){
 
-  }*/
-
-  in_fd = open("server-to-client", O_RDONLY);
-  char * recc = (char *) malloc(sizeof(char));
-  printf("Client really hopeful\n");
-  read("server-to-client", recc, sizeof(recc));
-  printf("Client recieved pipe\n");
-  if (strcmp(recc, "recieved server-to-client") == 0) {
-    write("server-to-client", "recieved client-to-server", sizeof("recieved client-to-server"));
-  }
-  return getpid();
+  //Step 5
+  int to_server = open(send, O_RDONLY);
+  char recc[MESSAGE_BUFFER_SIZE];
+  //Step 8
+  read(to_server, recc, MESSAGE_BUFFER_SIZE);
+  printf("Client recieved pipe: %s\n", recc);
+  remove(send);
+  //Step 9
+  write(*from_client, "Connected", sizeof("Connected"));
+  return to_server;
 }
